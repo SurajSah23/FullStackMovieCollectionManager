f189a80f8e19ff5388a5660311ff4cdaf09704e4
Suraj Sah <166237181+SurajSah23@users.noreply.github.com>
2024-12-29 20:30:50 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://full-stack-movie-collection-manager.vercel.app/api'
 });
 
 export const getMovies = () => api.get('/movies');
